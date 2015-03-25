611d0634c41a60b4bed7dc47b674cbdc9db36aaf
ashish <jindal1991@gmail.com>
2015-03-25 23:23:26 +0530

    Update package name.

---
+++ coho-webapp/src/main/java/com/cagiris/coho/controller/userprofile/Profile.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015, Cagiris Pvt. Ltd.
+ * All rights reserved.
+ */
+package com.cagiris.coho.controller.userprofile;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Ashish Jindal
+ *
+ */
+@Controller
+public class Profile {
+
+	@RequestMapping(value = {"/profile"}, method = RequestMethod.GET)
+	public String showProfilePage () {
+		return "profile";
+	}
+}
