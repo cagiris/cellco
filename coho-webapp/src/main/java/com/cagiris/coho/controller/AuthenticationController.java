e44d3c56bebf8b0713df04553163ceed0cee9ea0
ashish <jindal1991@gmail.com>
2015-04-08 20:35:58 +0530

    Authentication error messages.

--- coho-webapp/src/main/java/com/cagiris/coho/controller/AuthenticationController.java
+++ coho-webapp/src/main/java/com/cagiris/coho/controller/AuthenticationController.java
@@ -9,8 +9,10 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Ashish Jindal
@@ -22,12 +24,16 @@ public class AuthenticationController extends AbstractController {
 	private static Logger logger = Logger.getLogger(AuthenticationController.class);
 
 	@RequestMapping(value = {"/login"}, method = RequestMethod.GET)
-	public String showLoginPage () {
+	public String showLoginPage (@RequestParam (required = false) String error, ModelMap model) {
 		
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		if (!(auth instanceof AnonymousAuthenticationToken)) {
-			logger.debug("The user is already logged in");
-		    return "redirect:/";
+		if ((error != null) && error.equals("true")) {
+			model.addAttribute("error", "true");
+		} else {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			if (!(auth instanceof AnonymousAuthenticationToken)) {
+				logger.debug("The user is already logged in");
+			    return "redirect:/";
+			}
 		}
 		
 		return "login";
--- coho-webapp/src/main/webapp/WEB-INF/views/partials/common/main-content/login.jsp
+++ coho-webapp/src/main/webapp/WEB-INF/views/partials/common/main-content/login.jsp
@@ -2,6 +2,12 @@
 <form class="form-signin" action="<core:url value = '/authenticate' />" method="POST">
 	<h3 class="form-signin-heading">Please sign in</h3>
 	
+	<core:if test="${not empty error}">
+    	<!-- Display error message -->
+    	<p class="error-msg">* ${sessionScope["SPRING_SECURITY_LAST_EXCEPTION"].message}</p>
+		<div class="clearfix"></div>
+	</core:if>
+	
 	<label for="userName" class="sr-only">User Name</label>
 	<input name="userName" type="text" id="userName" class="form-control" placeholder="User Name" required autofocus> 
 	
