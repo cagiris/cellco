de88f15bb9b059d049f4be0eaa87e9ed9943d46c
Sanjeev Kumar <sanjeev90an@gmail.com>
2015-04-10 23:15:51 +0530

    json util added

---
+++ CohoService/src/main/java/com/cagiris/coho/service/utils/JSONUtils.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2015, Cagiris Pvt. Ltd.
+ * All rights reserved.
+ */
+package com.cagiris.coho.service.utils;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.cagiris.coho.service.exception.CohoException;
+
+/**
+ *
+ * @author: ssnk
+ */
+
+public class JSONUtils {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String getJsonStringForObject(Object object) throws CohoException {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (JsonGenerationException e) {
+            throw new CohoException(e);
+        } catch (JsonMappingException e) {
+            throw new CohoException(e);
+        } catch (IOException e) {
+            throw new CohoException(e);
+        }
+    }
+}
